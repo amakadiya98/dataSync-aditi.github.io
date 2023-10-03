77983bf7a6a0b812e5761e88cd598c1e1a9f15d9
aditi <amakadiya98@gmail.com>
2023-10-03 18:01:49 +0530

    add remaining pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -41,6 +41,23 @@ $('.opacity-ref-slider').owlCarousel({
         },
     }
 })
+$('.bday-girl-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ["<img src='./assets/images/prev-img.png'>", "<img src='./assets/images/next-img.png'>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 
 
 $(() => {
